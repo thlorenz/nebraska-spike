c5b31d116c81ddaadfc6d07ff2fdca5eac77ba28
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-09 15:08:37 -0700

    separating out transforms and readable

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var util                   =  require('util')
   , Readable               =  stream.Readable
   , Writable               =  stream.Writable
   , xtend                  =  require('xtend')
+  , BlessTransform         =  require('./lib/bless-transform')
+  , BlessedRenderTransform =  require('./lib/blessed-render-transform')
+  , WatcherReadable        =  require('./lib/watcher-readable')
   , BlessedRenderTransform =  require('./lib/blessed-render-transform')
   , formatReport           =  require('./lib/format-report')
   ;
@@ -17,21 +20,6 @@ var defaultOpts = {
     } 
   , format: formatReport
 };
-
-var Transform =  stream.Transform
-util.inherits(BlessTransform, Transform);
-
-function BlessTransform (opts) {
-  Transform.call(this, opts);
-  this._blessed = opts.blessed;
-}
-
-BlessTransform.prototype._transform = function (val, enc, cb) {
-  this._blessed.blessedValue = val;
-  this.push(this._blessed);
-  cb();
-}
-
 
 function renderStreamState(stream, opts) {
   opts = opts || {};
@@ -46,72 +34,6 @@ function renderStreamState(stream, opts) {
     .pipe(new BlessedRenderTransform(opts))
 }
 
-util.inherits(WatcherReadable, Readable);
-function WatcherReadable (stream, opts) { 
-  if (!(this instanceof WatcherReadable)) return new WatcherReadable(opts);
-
-  opts = opts || {};
-  opts.objectMode = true;
-  Readable.call(this, opts);
-  this._interval = opts.interval || 500;
-
-  var info = {};
-  if (stream._readableState) info.readable = stream._readableState;
-  if (stream._writableState) info.writable = stream._writableState;
-  this._label = (opts.blessed && opts.blessed.label) || stream.constructor.name + '(W)';
-  this._streamInfo = info;
-}
-
-var proto = WatcherReadable.prototype;
-
-proto._read = function () {
-  setTimeout(this._report.bind(this), this._interval);
-}
-
-proto._report = function () {
-  var info = this._streamInfo;
-  var r = { label: this._label };
-
-  if (info.readable) r.readable = this._reportReadable(info.readable);
-  if (info.writable) r.writable = this._reportWritable(info.writable);
-  r.index = info.index;
-
-  this.push(r);
-}
-
-proto._reportReadable = function (readable) {
-  var report = {};
-
-  [ 'highWaterMark'
-  //, 'objectMode'
-  //, 'flowing'
-  //, 'pipesCount'
-  //, 'reading' 
-  //, 'ranOut'
-  //, 'awaitDrain'
-  ].forEach(reportOn);
-
-  function reportOn (k) {
-    report[k] = readable[k];
-  }
-
-  report.bufferLength = readable.buffer.length;
-  return report;
-}
-
-proto._reportWritable = function (writable) {
-  var report = {};
-
-  [ 'highWaterMark'
-  ].forEach(reportOn);
-
-  function reportOn (k) {
-    report[k] = writable[k];
-  }
-
-  report.bufferLength = writable.buffer.length;
-  return report;
-}
 
 function offsetRender(stream, x, y) {
   renderStreamState (
@@ -127,8 +49,8 @@ function offsetRender(stream, x, y) {
 // Test
 if (!module.parent) {
   var NumberReadable    =  require('./test/util/number-readable')
-    , ThrottleTransform =  require('./test/util/throttle-transform')
     , PowerTransform    =  require('./test/util/power-transform')
+    , ThrottleTransform =  require('./lib/throttle-transform')
     , DevNullWritable   =  require('./lib/dev-null-writable')
     , tap               =  require('tap-stream')
     ;
@@ -138,7 +60,7 @@ if (!module.parent) {
 
   // unblocks the eventloop for one turn to allow rendering to happen 
   var minThrottle  =  new ThrottleTransform();
-  var longThrottle =  new ThrottleTransform({ throttle: 100 });
+  var longThrottle =  new ThrottleTransform({ throttle: 2000 });
   var devnull      =  new DevNullWritable();
 
   var numberRender = new BlessedRenderTransform({
@@ -170,5 +92,5 @@ if (!module.parent) {
 
   offsetRender(numbers, 0, 0)
   offsetRender(longThrottle, 40, 0)
-  offsetRender(powers, 0, 15)
+  offsetRender(powers, 0, 20)
 }
